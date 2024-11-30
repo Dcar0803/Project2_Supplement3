46822501bbbb2600f2c047fe9e1a41cc417364ed
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-30 14:22:44 -0500

    Test: Created a test function for binary search a sorted list.

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -5,8 +5,23 @@ import org.junit.jupiter.api.Test;
 class Testing {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	void testBinarySearch() {
+		int[] sortedArray = {1, 3, 5,7,9,};
+		
+		assertEquals(2, Main.binarySearch(sortedArray, 5)); // Element 5 is at index 2
+		
+		assertEquals(-1, Main.binarySearch(sortedArray, 6)); // Element 6 is not in the array
+		
+		assertEquals(0, Main.binarySearch(sortedArray, 1)); // First element
+        assertEquals(4, Main.binarySearch(sortedArray, 9)); // Last element
+
+        // Test: Empty array
+        assertEquals(-1, Main.binarySearch(new int[]{}, 5)); // Array is empty
+
+        // Test: Single-element array
+        assertEquals(0, Main.binarySearch(new int[]{10}, 10)); // Element matches
+        assertEquals(-1, Main.binarySearch(new int[]{10}, 5)); // Element does not match
+        
+	}//end of testBinarySearch function
 
 }//end of Testing class
